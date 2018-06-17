ab1fd9c836b85ef2af1b83211d43f3360aacda2a
sergeyt <stodyshev@gmail.com>
2018-06-17 11:46:17 +0700

    fix cmdline argument

--- start-dgraph.cmd
+++ start-dgraph.cmd
@@ -1,4 +1,4 @@
 if not exist data mkdir data
 start dgraph zero --wal data/zw
-start dgraph server --memory_mb 2048 --wal data/w --postings data/p --debugmode --bindall
+start dgraph server --lru_mb 2048 --wal data/w --postings data/p --debugmode --bindall
 start dgraph-ratel
