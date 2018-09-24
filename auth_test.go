48c3ae6b52a6b1615dbdc19f5c4756ccd84b2ce1
sergeyt <stodyshev@gmail.com>
2018-09-24 12:46:35 +0700

    minor

--- auth_test.go
+++ auth_test.go
@@ -9,7 +9,8 @@ func TestAuth(t *testing.T) {
 	c := setup(t)
 	defer c.Close()
 
-	resp := c.expect.POST("/api/login").WithBasicAuth("admin", "admin123").
+	resp := c.expect.POST("/api/login").
+		WithBasicAuth("admin", "admin123").
 		Expect().
 		Status(http.StatusOK).
 		JSON()
