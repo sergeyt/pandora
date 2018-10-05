114867c56e132bf00280308e16abb4370a1adef9
sergeyt <stodyshev@gmail.com>
2018-10-05 23:21:06 +0700

    cleanup

--- apps/chat/src/index.js
+++ apps/chat/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import './index.css'
 import store from './store'
 import rootSaga from './saga'
 import App from './App'
