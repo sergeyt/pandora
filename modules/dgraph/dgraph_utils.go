460a0a40a0d9253d69a2f4def27fbb436d2c0857
sergeyt <stodyshev@gmail.com>
2018-12-09 21:58:48 +0700

    remove unused func

--- modules/dgraph/dgraph_utils.go
+++ modules/dgraph/dgraph_utils.go
@@ -69,30 +69,3 @@ func ReadNode(ctx context.Context, tx *dgo.Txn, id string) (map[string]interface
 
 	return d, nil
 }
-
-func queryKeys(ctx context.Context, tx *dgo.Txn, id string) ([]string, error) {
-	query := fmt.Sprintf(`{
-  keys(func: uid(%s)) {
-    _predicate_
-  }
-}`, id)
-
-	resp, err := tx.Query(ctx, query)
-	if err != nil {
-		return nil, err
-	}
-
-	var result struct {
-		Results []struct {
-			Keys []string `json:"_predicate_"`
-		} `json:"keys"`
-	}
-	err = json.Unmarshal(resp.GetJson(), &result)
-	if err != nil {
-		return nil, err
-	}
-	if len(result.Results) == 0 {
-		return nil, fmt.Errorf("not found")
-	}
-	return result.Results[0].Keys, nil
-}
