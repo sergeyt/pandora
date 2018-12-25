d6d70d2063d71b6d50aa913b3def6a9ac0338c88
sergeyt <stodyshev@gmail.com>
2018-12-26 06:44:04 +0700

    fix words init according to new schema

--- scripts/words.py
+++ scripts/words.py
@@ -93,32 +93,82 @@ def nquad(id, k, v):
     return "_:{0} <{1}> {2} .\n".format(id, p, s)
 
 
-def nquads(d, id):
+def nquads(d, id = 'x'):
     result = ''
     for k, v in d.iteritems():
         result += nquad(id, k, v)
     return result
 
+def get_id(resp, id = 'x'):
+    return resp['data']['uids'][id]
 
+def pairs(list):
+    for x in list:
+        for y in list:
+            yield (x, y)
+
+# TODO find existing words and update their transcriptions, pronunciations, images
+# TODO port this script to golang and provide an API endpoint
 for word in words:
+    # insert word nodes for all languages
+    nodes = {}
+    word_ids = []
+    for key in word:
+        if key.startswith('text@'):
+            text = word[key]
+            lang = key[key.index('@')+1:]
+            props = {
+                'text': text,
+                'lang': lang,
+            }
+            trans_key = 'transcription@' + lang
+            if trans_key in word:
+                props['transcription'] = word[trans_key]
+            data = nquads(props)
+            resp = api.set_nquads(data)
+            wid = get_id(resp)
+            nodes[lang] = wid
+            word_ids.append(wid)
+
+    # link words together with translated_as predicate
+    proc = {}
+    for (w1, w2) in pairs(word_ids):
+        k1 = "{0}-{1}".format(w1, w2)
+        k2 = "{0}-{1}".format(w2, w1)
+        if k1 in proc:
+            continue
+        data = "<{0}> <translated_as> <{1}> .\n".format(w1, w2)
+        api.set_nquads(data)
+        proc[k1] = 1
+        proc[k2] = 1
+
+    # todo upload images to S3 store
     # insert images
-    images = []
+    image_nodes = []
     for url in word['images']:
         data = nquads({
             'url': url,
             'source': 'google',
-        }, "img")
+        })
         resp = api.set_nquads(data)
-        images.append(resp['data']['uids']['img'])
+        obj_id = get_id(resp)
+        # link with connected words
+        for lang in nodes:
+            wid = nodes[lang]
+            data = "<{0}> <relevant> <{1}> .\n".format(wid, obj_id)
+            api.set_nquads(data)
 
-    # insert word
-    word.pop('images', None)
-    data = nquads(word, 'w')
-    resp = api.set_nquads(data)
-    wid = resp['data']['uids']['w']
-    
-    # link word with images
-    for img in images:
-        data = "<{0}> <image> <{1}> .\n".format(wid, img)
-        data += "<{0}> <word> <{1}> .\n".format(img, wid)
-        api.set_nquads(data)
+    # todo upload sounds to S3 store
+    for key in word:
+        if key.startswith('pronunciation@'):
+            lang = key[key.index('@')+1:]
+            data = nquads({
+                'url': word[key],
+                'source': 'https://howjsay.com',
+            })
+            resp = api.set_nquads(data)
+            obj_id = get_id(resp)
+            # link with word
+            wid = nodes[lang]
+            data = "<{0}> <pronounced_as> <{1}> .\n".format(wid, obj_id)
+            api.set_nquads(data)
