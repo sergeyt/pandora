6534cb72a9c550ec1adbc992ce49ec105b8bc8d3
sergeyt <stodyshev@gmail.com>
2019-01-20 15:51:36 +0700

    fixing upload test

--- scripts/upload.py
+++ scripts/upload.py
@@ -2,9 +2,15 @@
 
 import os
 from tusclient import client
+import utils
+
+utils.enable_logging_with_headers()
 
 dir = os.path.dirname(os.path.realpath(__file__))
+fname = os.path.realpath(os.path.join(dir, '../schema.txt'))
 
-tc = client.TusClient('http://localhost:4200/api/files/')
-uploader = tc.uploader(os.path.join(dir, '../schema.txt'), chunk_size=1024)
+my_client = client.TusClient('http://localhost:4200/api/files')
+my_client.set_headers({'Upload-Metadata': 'filename {0}'.format(os.path.basename(fname))})
+
+uploader = my_client.uploader(fname, chunk_size=1024)
 uploader.upload()
