f9d5cbeeb4a73122c61561f9ad5b550f1270dd0e
sergeyt <stodyshev@gmail.com>
2019-02-17 17:58:19 +0700

    minor rename

--- scripts/upload_tus.py
+++ scripts/upload_tus.py
@@ -7,11 +7,11 @@ import utils
 utils.enable_logging_with_headers()
 
 dir = os.path.dirname(os.path.realpath(__file__))
-fname = os.path.realpath(os.path.join(dir, '../schema.txt'))
+filename = os.path.realpath(os.path.join(dir, '../schema.txt'))
 
 my_client = client.TusClient('http://localhost:1080/api/files')
 # my_client.set_headers(
 #     {'Upload-Metadata': 'filename {0}'.format(os.path.basename(fname))})
 
-uploader = my_client.uploader(fname, chunk_size=1024)
+uploader = my_client.uploader(filename, chunk_size=1024)
 uploader.upload()
