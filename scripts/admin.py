b2e0765b8cdb2f3fcaa873d37db9df5edb896e07
sergeyt <stodyshev@gmail.com>
2019-02-24 02:50:34 +0700

    check admin secret

--- scripts/admin.py
+++ scripts/admin.py
@@ -6,19 +6,53 @@ import logging
 import traceback
 import api
 import initdata
+import words
 
-from flask import Flask
+from functools import wraps
+from flask import Flask, request
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.debug = True
+done = 'done!'
+
+
+def secret_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        secret = request.args.get('secret')
+        if secret is None or secret == '' or secret != os.getenv(
+                'ADMIN_SECRET'):
+            return 'bad secret!'
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@app.route('/api/pyadmin/dropall')
+@secret_required
+def dropall():
+    api.drop_all()
+    api.init_schema()
+    return done
+
+
+@app.route('/api/pyadmin/initschema')
+@secret_required
+def initschema():
+    api.drop_all()
+    api.init_schema()
+    return done
 
 
 @app.route('/api/pyadmin/resetdb')
+@secret_required
 def resetdb():
     api.drop_all()
     api.init_schema()
     initdata.init()
-    return 'done!'
+    words.init()
+    return done
 
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
--- scripts/words.py
+++ scripts/words.py
@@ -44,73 +44,78 @@ def pairs(list):
             yield (x, y)
 
 
-# TODO find existing words and update their transcriptions, pronunciations, images
-# TODO port this script to golang and provide an API endpoint
-for word in words:
-    # insert word nodes for all languages
-    nodes = {}
-    word_ids = []
-    for key in word:
-        if key.startswith('text@'):
-            text = word[key]
-            lang = key[key.index('@') + 1:]
-            props = {
-                '_word': '',
-                'text': text,
-                'lang': lang,
-            }
-            trans_key = 'transcription@' + lang
-            if trans_key in word:
-                props['transcription'] = word[trans_key]
-            data = nquads(props)
-            resp = api.set_nquads(data)
-            wid = get_id(resp)
-            nodes[lang] = wid
-            word_ids.append(wid)
+def init():
+    # TODO find existing words and update their transcriptions, pronunciations, images
+    # TODO port this script to golang and provide an API endpoint
+    for word in words:
+        # insert word nodes for all languages
+        nodes = {}
+        word_ids = []
+        for key in word:
+            if key.startswith('text@'):
+                text = word[key]
+                lang = key[key.index('@') + 1:]
+                props = {
+                    '_word': '',
+                    'text': text,
+                    'lang': lang,
+                }
+                trans_key = 'transcription@' + lang
+                if trans_key in word:
+                    props['transcription'] = word[trans_key]
+                data = nquads(props)
+                resp = api.set_nquads(data)
+                wid = get_id(resp)
+                nodes[lang] = wid
+                word_ids.append(wid)
 
-    # link words together with translated_as predicate
-    proc = {}
-    for (w1, w2) in pairs(word_ids):
-        if w1 == w2:
-            continue
-        k1 = "{0}-{1}".format(w1, w2)
-        k2 = "{0}-{1}".format(w2, w1)
-        if k1 in proc:
-            continue
-        data = "<{0}> <translated_as> <{1}> .\n".format(w1, w2)
-        api.set_nquads(data)
-        proc[k1] = 1
-        proc[k2] = 1
-
-    # todo upload images to S3 store
-    # insert images
-    image_nodes = []
-    for url in word['images']:
-        data = nquads({
-            '_image': '',
-            'url': url,
-            'source': 'google',
-        })
-        resp = api.set_nquads(data)
-        obj_id = get_id(resp)
-        # link with connected words
-        for lang in nodes:
-            wid = nodes[lang]
-            data = "<{0}> <relevant> <{1}> .\n".format(wid, obj_id)
+        # link words together with translated_as predicate
+        proc = {}
+        for (w1, w2) in pairs(word_ids):
+            if w1 == w2:
+                continue
+            k1 = "{0}-{1}".format(w1, w2)
+            k2 = "{0}-{1}".format(w2, w1)
+            if k1 in proc:
+                continue
+            data = "<{0}> <translated_as> <{1}> .\n".format(w1, w2)
             api.set_nquads(data)
+            proc[k1] = 1
+            proc[k2] = 1
 
-    # todo upload sounds to S3 store
-    for key in word:
-        if key.startswith('pronunciation@'):
-            lang = key[key.index('@') + 1:]
+        # todo upload images to S3 store
+        # insert images
+        image_nodes = []
+        for url in word['images']:
             data = nquads({
-                '_sound': '',
-                'url': word[key],
-                'source': 'https://howjsay.com',
+                '_image': '',
+                'url': url,
+                'source': 'google',
             })
             resp = api.set_nquads(data)
             obj_id = get_id(resp)
-            # link with word
-            wid = nodes[lang]
-            data = "<{0}> <pronounced_as> <{1}> .\n".format(wid, obj_id)
-            api.set_nquads(data)
+            # link with connected words
+            for lang in nodes:
+                wid = nodes[lang]
+                data = "<{0}> <relevant> <{1}> .\n".format(wid, obj_id)
+                api.set_nquads(data)
+
+        # todo upload sounds to S3 store
+        for key in word:
+            if key.startswith('pronunciation@'):
+                lang = key[key.index('@') + 1:]
+                data = nquads({
+                    '_sound': '',
+                    'url': word[key],
+                    'source': 'https://howjsay.com',
+                })
+                resp = api.set_nquads(data)
+                obj_id = get_id(resp)
+                # link with word
+                wid = nodes[lang]
+                data = "<{0}> <pronounced_as> <{1}> .\n".format(wid, obj_id)
+                api.set_nquads(data)
+
+
+if __name__ == '__main__':
+    init()
