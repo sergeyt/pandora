b4dc92e78dad10ca18151dac535aca4f8134522c
sergeyt <stodyshev@gmail.com>
2019-02-28 16:22:37 +0700

    script to download go

---
+++ scripts/dlgo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+GO_VERSION=1.12
+FILE=go${GO_VERSION}.linux-amd64.tar.gz
+
+curl -O https://storage.googleapis.com/golang/${FILE}
+tar -xvf ${FILE}
+rm -rf ${HOME}/tools/go
+mv go ${HOME}/tools
+rm -f ${FILE}
