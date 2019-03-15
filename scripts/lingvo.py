e0222ed9a2167bc8338ecce627d187ef43c458da
sergeyt <stodyshev@gmail.com>
2019-03-15 07:58:40 +0700

    audio from howjsay and macmillan dictionaries

--- scripts/lingvo.py
+++ scripts/lingvo.py
@@ -3,6 +3,7 @@
 import os
 import re
 import api
+import macmillan
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,7 +11,7 @@ dir = os.path.dirname(os.path.realpath(__file__))
 def map_type(line):
     if line.startswith('_:img_'):
         return 'file'
-    if re.match("_:\w+_(en|ru)", line):
+    if re.match(r'_:\w+_(en|ru)', line):
         return 'term'
     return None
 
@@ -19,24 +20,56 @@ def idof(line):
     return line.split(' ')[0]
 
 
+def add_audio(line, id, buf, audio):
+    m = re.match(r'_:(\w+)_en', line)
+    if m is None:
+        return
+
+    word = m.group(1)
+    
+    m = macmillan.find_audio(word)
+    src2 = 'https://www.macmillandictionary.com'
+    lines = [
+        '_:aud_{0}1 <url> "https://howjsay.com/mp3/{0}.mp3" .'.format(word),
+        '_:aud_{0}1 <source> "https://howjsay.com" .'.format(word),
+        '_:aud_{0}2 <url> "{1}" .'.format(word, m['mp3']),
+        '_:aud_{0}2 <source> "{1}" .'.format(word, src2),
+    ]
+    for t in lines:
+        buf.append(t)
+    buf.append('')
+
+    audio[id] = []
+    for i in range(1, 3):
+        audio[id].append('{0} <audio> _:aud_{1}{2} .'.format(id, word, i))
+
+
 def init():
     with open(os.path.join(dir, 'lingvo.txt'), 'r', encoding='utf-8') as f:
         src = f.read().split('\n')
         buf = []
         typed = {}
+        audio = {}
         for line in src:
-            buf.append(line)
             kind = map_type(line)
-            if kind:
-                id = idof(line)
-                if id not in typed:
-                    buf.append('{0} <_{1}> "" .'.format(id, kind))
-                    typed[id] = True
+            id = idof(line)
+            if kind and id not in typed:
+                add_audio(line, id, buf, audio)
+                buf.append('{0} <_{1}> "" .'.format(id, kind))
+                typed[id] = True
+
+            if "<visual>" in line and id in audio:
+                for t in audio[id]:
+                    buf.append(t)
+
+            buf.append(line)
+
             if "<visual>" in line:
                 buf.append(line.replace("<visual>", "<relevant>"))
                 buf.append(line.replace("<visual>", "<related>"))
 
     data = '\n'.join(buf)
+    print(data)
     api.set_nquads(data)
 
 
