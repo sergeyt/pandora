01c34ef25ce9a848d1553128c5ee70ba52010372
sergeyt <stodyshev@gmail.com>
2019-03-20 23:17:10 +0700

    fix upload test

--- scripts/test_upload.py
+++ scripts/test_upload.py
@@ -7,59 +7,61 @@ import requests
 def test_upload():
     api.login("admin", "admin123")
     headers = api.headers()
+    params = {'key': api.API_KEY}
 
     file_url = api.url('/api/file/schema.txt')
 
     files = {'file': open(api.schema_path(), 'rb')}
-    resp = requests.post(file_url, headers=headers, files=files)
+    resp = requests.post(file_url, params=params, headers=headers, files=files)
     resp.raise_for_status()
 
     node = resp.json()
     file_id = node['uid']
 
     files = {'file': open(api.schema_path(), 'rb')}
-    resp = requests.put(file_url, headers=headers, files=files)
+    resp = requests.put(file_url, params=params, headers=headers, files=files)
     resp.raise_for_status()
 
     # download by path
-    resp = requests.get(file_url, headers=headers)
+    resp = requests.get(file_url, params=params, headers=headers)
     resp.raise_for_status()
     print(resp.text)
 
     # download by id
     url1 = api.url('/api/file/{0}'.format(file_id))
-    resp = requests.get(url1, headers=headers)
+    resp = requests.get(url1, params=params, headers=headers)
     resp.raise_for_status()
     print(resp.text)
 
-    resp = requests.delete(file_url, headers=headers)
+    resp = requests.delete(file_url, params=params, headers=headers)
     resp.raise_for_status()
 
 
 def test_delete_file_node():
     api.login("admin", "admin123")
     headers = api.headers()
+    params = {'key': api.API_KEY}
 
     file_url = api.url('/api/file/schema2.txt')
 
     files = {'file': open(api.schema_path(), 'rb')}
-    resp = requests.post(file_url, headers=headers, files=files)
+    resp = requests.post(file_url, params=params, headers=headers, files=files)
     resp.raise_for_status()
 
     node = resp.json()
     file_id = node['uid']
 
     # download by path
-    resp = requests.get(file_url, headers=headers)
+    resp = requests.get(file_url, params=params, headers=headers)
     resp.raise_for_status()
     print(resp.text)
 
     # download by id
     url1 = api.url('/api/file/{0}'.format(file_id))
-    resp = requests.get(url1, headers=headers)
+    resp = requests.get(url1, params=params, headers=headers)
     resp.raise_for_status()
     print(resp.text)
 
     url2 = api.url('/api/data/file/{0}'.format(file_id))
-    resp = requests.delete(url2, headers=headers)
+    resp = requests.delete(url2, params=params, headers=headers)
     resp.raise_for_status()
