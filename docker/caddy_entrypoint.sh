f8198cb3af6fd9a548a031aa23bfab7fc054f99b
sergeyt <stodyshev@gmail.com>
2019-03-23 22:31:44 +0700

    increase timeout

--- docker/caddy_entrypoint.sh
+++ docker/caddy_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
-cd /pyadmin && gunicorn --daemon --workers 2 --timeout 120 --graceful-timeout 30 --log-level DEBUG admin:app &
+cd /pyadmin && gunicorn --daemon --workers 2 --timeout 300 --graceful-timeout 30 --log-level DEBUG admin:app &
 
 caddy -email stodyshev@gmail.com -agree --conf /etc/caddy/Caddyfile
