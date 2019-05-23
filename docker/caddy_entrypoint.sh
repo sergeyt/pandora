28cd4e88408e2c2846d3971b129c8673eab8bad4
sergeyt <stodyshev@gmail.com>
2019-05-23 10:04:13 +0700

    run pyadmin in separate container

--- docker/caddy_entrypoint.sh
+++ docker/caddy_entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -e
 
-cd /pyadmin && gunicorn --timeout 900 --graceful-timeout 30 --log-level DEBUG admin:app &
-
 caddy -email stodyshev@gmail.com -agree --conf /etc/caddy/Caddyfile
---
+++ docker/pyadmin_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+cd /pyadmin
+
+FLASK_PORT=${FLASK_PORT:-3001}
+FLASK_ENV=${FLASK_ENV:-development}
+FLASK_WORKERS=${FLASK_WORKERS:-2}
+
+export FLASK_ENV=${FLASK_ENV}
+export FLASK_APP=admin.py
+
+if [[ "$FLASK_ENV" = "development" ]]; then
+    python -m flask run --host 0.0.0.0 --port ${FLASK_PORT}
+else
+    gunicorn --bind :${FLASK_PORT} --workers ${FLASK_WORKERS} --timeout 900 --graceful-timeout 30 --log-level DEBUG admin:app
+fi
