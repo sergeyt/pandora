5a886b0d2a601bb4912c0647538a2bd3de9ddd36
sergeyt <stodyshev@gmail.com>
2019-06-22 22:54:44 +0700

    test delete file by id

--- scripts/test_upload.py
+++ scripts/test_upload.py
@@ -29,8 +29,8 @@ def test_upload():
     print(resp.text)
 
     # download by id
-    url1 = api.url('/api/file/{0}'.format(file_id))
-    resp = requests.get(url1, params=params, headers=headers)
+    file_url2 = api.url('/api/file/{0}'.format(file_id))
+    resp = requests.get(file_url2, params=params, headers=headers)
     resp.raise_for_status()
     print(resp.text)
 
@@ -64,5 +64,35 @@ def test_delete_file_node():
     print(resp.text)
 
     url2 = api.url('/api/data/file/{0}'.format(file_id))
+    resp = requests.delete(url2, params=params, headers=headers)
+    resp.raise_for_status()
+
+
+def test_delete_file_by_id():
+    api.login("system", os.getenv("SYSTEM_PWD"))
+    headers = api.headers()
+    params = {'key': api.API_KEY}
+
+    file_url = api.url('/api/file/schema3.txt')
+
+    files = {'file': open(api.schema_path(), 'rb')}
+    resp = requests.post(file_url, params=params, headers=headers, files=files)
+    resp.raise_for_status()
+
+    node = resp.json()
+    file_id = node['uid']
+
+    # download by path
+    resp = requests.get(file_url, params=params, headers=headers)
+    resp.raise_for_status()
+    print(resp.text)
+
+    # download by id
+    url1 = api.url('/api/file/{0}'.format(file_id))
+    resp = requests.get(url1, params=params, headers=headers)
+    resp.raise_for_status()
+    print(resp.text)
+
+    url2 = api.url('/api/file/{0}'.format(file_id))
     resp = requests.delete(url2, params=params, headers=headers)
     resp.raise_for_status()
