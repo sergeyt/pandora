396408eafed6f5d87b1f593ab059b79e66e1da53
Sergey Todyshev <stodyshev@gmail.com>
2020-01-12 21:03:19 +0300

    enable air live reload

--- docker/app_main.sh
+++ docker/app_main.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 cd /pandora
-cat .air.conf
 go install
-dockerize --wait tcp://dgraph:9080 --wait tcp://minio:9000 --wait tcp://nats:4222 pandora
+dockerize --wait tcp://dgraph:9080 --wait tcp://minio:9000 --wait tcp://nats:4222 /usr/local/bin/air
