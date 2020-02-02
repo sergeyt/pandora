42880d382c4a127fe5ca04c02b9b7395e0e768fc
rom <asromahin@yandex.ru>
2020-02-02 14:10:12 +0700

    add parse macmillan

--- scripts/macmillan.py
+++ scripts/macmillan.py
@@ -1,38 +1,90 @@
 #!/usr/bin/env python
 
-import re
 import requests
 import json
 from urllib.parse import quote
 import utils
-from models import File
+
+from bs4 import BeautifulSoup
+from models import File, Term
 
 NAME = 'macmillan'
 
 first = lambda a: next(iter(a or []), None)
 
 
-def get_data(text, lang='en'):
+def stripped_text(node):
+    if node is None:
+        return None
+    return node.get_text().strip()
+
+
+def get_data(query, lang):
     if lang != 'en':
         return None
 
-    url = f'https://www.macmillandictionary.com/dictionary/british/{quote(text)}'
-    headers = {
-        'User-Agent': 'script',
-        'Accept': 'text/html',
+    data = {
+        'audio': [],
+        #'visual': [],
+        'tag': [],
+        'transcription': [],
+        'definition': [],
+        'in': [],
+        'synonym': [],
+        #'antonym': [],
+        #'related': []
     }
+
+    pat = 'https://www.macmillandictionary.com/dictionary/british/{0}'
+    url = pat.format(query)
+    headers = {
+            'User-Agent': 'script',
+            'Accept': 'text/html',
+        }
     resp = requests.get(url, headers=headers)
     resp.raise_for_status()
 
-    mp3 = first(re.findall('data-src-mp3="([^"]+)"', resp.text))
-    ogg = first(re.findall('data-src-ogg="([^"]+)"', resp.text))
-    if mp3 is None and ogg is None:
-        return None
+    soup = BeautifulSoup(resp.text, 'html.parser')
 
-    data = {'audio': []}
-    for url in [mp3, ogg]:
-        data['audio'].append(File(url=url, region=None))
+    #get transcription
+    transcriptions=soup.find_all(class_='PRON')
+    for t in transcriptions:
+      data['transcription'].append(Term(text=stripped_text(t).replace('/',''),lang=lang, region=None))
 
+    #get tags
+    crop_text=stripped_text(soup.find(class_='zwsp'))
+    part_speech=stripped_text(soup.find(class_='PART-OF-SPEECH')).replace(crop_text,'')
+    syntax_coding=stripped_text(soup.find(class_='SYNTAX-CODING'))
+    
+    data['tag'].append(Term(text=part_speech,lang=lang, region=None))
+    data['tag'].append(Term(text=syntax_coding,lang=lang, region=None))
+
+    #get defenition
+    defenitions=soup.find_all(class_='DEFINITION')
+    for d in defenitions:
+      data['definition'].append(Term(text=stripped_text(d),lang=lang, region=None))
+
+    #get examples
+    examples=soup.find_all(class_='EXAMPLES')
+    for e in examples:
+      data['in'].append(Term(text=stripped_text(e),lang=lang, region=None))
+    examples=soup.find_all(class_='PHR-XREF')
+    for e in examples:
+      data['in'].append(Term(text=stripped_text(e),lang=lang, region=None))
+
+    #get synonyms
+    synonyms=soup.find_all(class_='synonyms')
+    for allsyn in synonyms:
+      subsynonyms=allsyn.find_all(class_='theslink')
+      for syn in subsynonyms:
+        if(not '...' in syn.text):
+          data['synonym'].append(Term(text=stripped_text(syn),lang=lang, region=None))
+
+    #get audio
+    audio=soup.find(class_='audio_play_button')
+    data['audio'].append(File(url=audio['data-src-mp3'], region=None))
+    data['audio'].append(File(url=audio['data-src-ogg'], region=None))
+    
     return data
 
 
