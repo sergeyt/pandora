427a2edef39950c4e215cd6678e4914e7c72f56e
Sergey Todyshev <stodyshev@gmail.com>
2020-02-08 09:15:18 +0300

    structured logging

--- modules/api/api.go
+++ modules/api/api.go
@@ -9,14 +9,25 @@ import (
 	"github.com/go-chi/cors"
 	"github.com/gorilla/handlers"
 	"github.com/sergeyt/pandora/modules/auth"
+	"github.com/sirupsen/logrus"
 )
 
 // NewHandler makes http.Handler to serve HTTP API
 func NewHandler() http.Handler {
+	// Setup the logger backend using sirupsen/logrus and configure
+	// it to use a custom JSONFormatter. See the logrus docs for how to
+	// configure the backend at github.com/sirupsen/logrus
+	logger := logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{
+		// disable, as we set our own
+		DisableTimestamp: true,
+	}
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
-	r.Use(logMidleware)
+	// TODO configure log type (json | apache)
+	r.Use(NewStructuredLogger(logger))
 	r.Use(middleware.Recoverer)
 
 	// Basic CORS
@@ -30,6 +41,11 @@ func NewHandler() http.Handler {
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	})
 	r.Use(cors.Handler)
+
+	// for testing purposes
+	r.Get("/api/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("oops")
+	})
 
 	r.Group(healthAPI)
 	r.Group(auth.RegisterAPI)
