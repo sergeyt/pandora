879e247217fd942275df641c3a2b70d7e5ed4466
Sergey Todyshev <stodyshev@gmail.com>
2020-03-12 22:35:27 +0300

    push to dgraph

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -3,17 +3,7 @@ import requests
 from celery import Celery
 from elasticsearch import Elasticsearch
 from worker import app
-
-TIKA_HOST = os.getenv('TIKA_HOST', 'http://localhost:4219')
-ES_HOSTS = os.getenv('ES_HOSTS', 'localhost:9200')
-ES_INDEX = os.getenv('ES_INDEX_DOCS', 'docs')
-
-es = Elasticsearch(hosts=ES_HOSTS)
-
-# ensure ES index
-if not es.indices.exists(ES_INDEX):
-    # TODO configure mappings
-    es.indices.create(ES_INDEX)
+import api
 
 
 # just for testing purposes
@@ -25,16 +15,16 @@ def add(x, y):
 # TODO update existing doc found by url
 @app.task
 def index_file(url):
+    print(f'indexing ${url}')
     # parse file by given URL
     resp = requests.get(url=TIKA_HOST + '/api/tika/parse', params={'url': url})
     resp.raise_for_status()
     result = resp.json()
 
-    # push metadata and text to elasticsearch
+    # TODO find existing doc by url
+
     doc = result['metadata']
     doc['source_url'] = url
     doc['text'] = result['text']
-    es.index(ES_INDEX, body=doc)
 
-    # TODO push dgraph data
-    return result
+    return api.post('/api/data/doc', doc)
