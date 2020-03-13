e8f494b7b62f1ac18715cf876c0f53b419e64a78
Sergey Todyshev <stodyshev@gmail.com>
2020-03-13 12:32:33 +0300

    decode json string

--- scripts/reactor.py
+++ scripts/reactor.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import json
 from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 from tasks import index_file
@@ -14,13 +15,15 @@ async def run(loop):
     async def message_handler(msg):
         subject = msg.subject
         reply = msg.reply
-        data = msg.data.decode()
-        print("received a message on '{subject} {reply}': {data}".format(
-            subject=subject, reply=reply, data=data))
+        data = json.loads(msg.data.decode())
 
-        if 'resource_type' in data and 'url' in data and data[
-                'resource_type'] == 'file':
-            url = data['url']
+        print(f"received a message on '${subject} ${reply}': ${data}")
+
+        resouce_type = data.get('resource_type', '')
+
+        if resouce_type == 'file':
+            url = data.get('url')
+            print(f'processing file ${url}')
             index_file.delay(url)
 
     await nc.subscribe("global", cb=message_handler)
