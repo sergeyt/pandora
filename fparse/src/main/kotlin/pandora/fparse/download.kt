04fc39703d992156ef6fcf1e1e27576f783542ca
Sergey Todyshev <stodyshev@gmail.com>
2020-03-14 21:27:20 +0300

    support local files for testing

--- fparse/src/main/kotlin/pandora/fparse/download.kt
+++ fparse/src/main/kotlin/pandora/fparse/download.kt
@@ -1,26 +1,38 @@
 package pandora.fparse
 
+import org.slf4j.LoggerFactory
+import org.springframework.core.io.FileUrlResource
 import org.springframework.core.io.Resource
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.web.client.RestTemplate
+import java.io.File
 import java.net.MalformedURLException
 import java.net.URL
-import org.slf4j.LoggerFactory;
+import java.nio.file.Files
 
 val LOGGER = LoggerFactory.getLogger(Application::class.java)
 
 fun downloadFile(url: String): ResponseEntity<Resource> {
+    val file = toAbsoluteUrl(url)
+
+    LOGGER.info("downloading file {}", file)
+
+    // WARNING for testing purposes only
+    // TODO in production allow only specific directories
+    if (file.startsWith("file://")) {
+        val path = File(file.removePrefix("file://")).toPath()
+        val contentType = Files.probeContentType(path)
+        val headers = HttpHeaders()
+        headers["Content-Type"] = contentType
+        val resource = FileUrlResource(path.toString())
+        return ResponseEntity(resource, headers, HttpStatus.OK)
+    }
+
     val rest = RestTemplate()
     val headers = HttpHeaders()
     headers.add("Accept", "*/*")
 
     val req = HttpEntity("", headers)
-    val file = toAbsoluteUrl(url)
-
-    LOGGER.info("downloading file {}", file)
 
     val res = rest.exchange(file, HttpMethod.GET, req, Resource::class.java)
     if (res.body == null) {
