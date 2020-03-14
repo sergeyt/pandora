f8fd4c8a25f77f38c816fad2f6fd48b211061f83
Sergey Todyshev <stodyshev@gmail.com>
2020-03-14 18:27:59 +0300

    system role

--- modules/auth/auth.go
+++ modules/auth/auth.go
@@ -86,7 +86,7 @@ func Middleware(next http.Handler) http.Handler {
 				Admin: true,
 				Claims: map[string]interface{}{
 					"email": email,
-					"role":  role,
+					"role":  "system",
 				},
 			}
 
