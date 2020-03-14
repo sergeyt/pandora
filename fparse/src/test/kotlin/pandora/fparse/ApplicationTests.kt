762ef42da179fe9af3cb608371eefbbf65d8a171
Sergey Todyshev <stodyshev@gmail.com>
2020-03-14 21:27:35 +0300

    parse books

--- fparse/src/test/kotlin/pandora/fparse/ApplicationTests.kt
+++ fparse/src/test/kotlin/pandora/fparse/ApplicationTests.kt
@@ -2,6 +2,8 @@ package pandora.fparse
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
+import java.nio.file.Files
 
 val aliceUrl = "https://www.adobe.com/be_en/active-use/pdf/Alice_in_Wonderland.pdf"
 
@@ -16,6 +18,21 @@ class ApplicationTests {
         assert(creator is String)
         assert((creator as String).startsWith("Lewis Carroll"))
         assert(result.text.strip().startsWith("BY LEWIS CARROLL"))
+    }
+
+    @Test
+    fun parseBooks() {
+        val dir = "/Users/admin/Dropbox/books"
+        val keys = HashSet<String>()
+        Files.list(File(dir).toPath()).forEach {
+            val ctrl = ParseController()
+            val result = ctrl.parse("file://" + it)
+            keys.addAll(result.metadata.keys)
+            println(result.metadata)
+        }
+        keys.forEach {
+            println(it)
+        }
     }
 
     @Test
